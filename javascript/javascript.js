160ae4ff725d6d2dd1650da21bcbe58a14ce6bfc
HVPasindu <hvpmjayarathna2001@gmail.com>
2026-01-11 14:57:37 +0530

    add customer page

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,12 +7,36 @@ function Login() {
 
     if (in_name === name && in_password === password) {
         alert("you are login scussusfull");
-        window.location.href ="addChild.html"
+        window.location.href = "addChild.html"
     } else {
         alert("wrong data enter,try again....")
     }
 
     document.getElementById('email').value = '';
     document.getElementById('password').value = '';
+
+}
+
+
+
+var customer = [];
+function Add() {
+    var nic = document.getElementById('nic').value;
+    var name = document.getElementById('Name').value;
+    var address = document.getElementById('Address').value;
+    var salary = document.getElementById('Salary').value;
+
+    var salary = parseFloat(salary);
+
+    var cus = {
+        nic :nic,
+        name: name,
+        address: address,
+        salary: salary,
+
+    }
+
+    customer.push(cus);
+    console.log(customer);
 
 }
